5309eb9320b184adcd5a4999332f8f9500413ec6
billyboy35 <kevin.niglaut@wanadoo.fr>
2025-02-02 22:58:47 +0100

    Fixe update prod SMEWebify/WebErpMesv2/issues/607

--- app/Http/Requests/Products/UpdateProductsRequest.php
+++ app/Http/Requests/Products/UpdateProductsRequest.php
@@ -25,7 +25,7 @@ class UpdateProductsRequest extends FormRequest
     {
         return [
             'label' => 'required|string|max:255',
-            'ind' => 'required|string',
+            'ind' => 'nullable|string',
             'methods_services_id' => 'required|integer',
             'methods_families_id' => 'required|integer',
             'purchased_price' => 'nullable|numeric',
