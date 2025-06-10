ae0a3ce5ef1c37dfaba719f94b21cb1f7e6ee4e8
fadeyim <44692257+fadeyim@users.noreply.github.com>
2025-06-10 13:15:47 -0600

    Customize iframe loading screen

--- resources/views/iframe-mode.blade.php
+++ resources/views/iframe-mode.blade.php
@@ -10,7 +10,19 @@
 @stop
 
 @section('css')
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 @stop
 
 @section('js')
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var loadingDiv = document.querySelector('.tab-loading > div');
+            if (loadingDiv) {
+                loadingDiv.innerHTML = `
+                    <img src="{{ asset('images/factory/1708549820_simple-logo.jpg') }}" class="iframe-logo" alt="Logo">
+                    <div class="iframe-progress"></div>
+                `;
+            }
+        });
+    </script>
 @stop
