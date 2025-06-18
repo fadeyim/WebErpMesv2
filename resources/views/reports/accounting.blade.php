c0fab7b8e395ab05d00f17f02e86259132070392
fadeyim <44692257+fadeyim@users.noreply.github.com>
2025-06-18 14:24:34 -0600

    Display accounting reports using iframe

--- resources/views/reports/accounting.blade.php
+++ resources/views/reports/accounting.blade.php
@@ -7,24 +7,5 @@
 @stop
 
 @section('content')
-<div class="row">
-    <div class="col-md-4">
-        <x-adminlte-card title="{{ __('general_content.total_revenue_trans_key') }}" theme="success" icon="fas fa-coins" >
-            {{ \Illuminate\Support\Number::currency($revenue, app('Factory')->curency, config('app.locale')) }}
-        </x-adminlte-card>
-    </div>
-    <div class="col-md-4">
-        <x-adminlte-card title="{{ __('general_content.total_expense_trans_key') }}" theme="danger" icon="fas fa-file-invoice-dollar" >
-            {{ \Illuminate\Support\Number::currency($expenses, app('Factory')->curency, config('app.locale')) }}
-        </x-adminlte-card>
-    </div>
-    <div class="col-md-4">
-        <x-adminlte-card title="{{ __('general_content.total_profit_trans_key') }}" theme="primary" icon="fas fa-chart-line" >
-            {{ \Illuminate\Support\Number::currency($profit, app('Factory')->curency, config('app.locale')) }}
-        </x-adminlte-card>
-    </div>
-</div>
-<div class="mt-4">
-    @livewire('fec-export-lines')
-</div>
+    <iframe src="http://msi/Reports/browse/Accounting%20Reports" style="width: 100%; height: 80vh; border: none;"></iframe>
 @stop
